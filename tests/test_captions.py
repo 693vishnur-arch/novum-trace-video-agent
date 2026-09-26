10c5714643d9b78c626a793290be351714cae438
Vishnu Radhakrishnan <693vishnur@gmail.com>
2026-09-26 20:24:05 +0400

    Correct ASS line-break test

--- tests/test_captions.py
+++ tests/test_captions.py
@@ -43,6 +43,6 @@ def test_build_ass_splits_long_scene_without_truncation(tmp_path):
 
     body_events = [line for line in ass.splitlines() if ",Caption," in line]
     assert len(body_events) >= 2
-    flattened = ass.replace(r"\\N", " ")
+    flattened = ass.replace("\\N", " ")
     assert "turn the servers off." in flattened
     assert "..." not in ass
